2b4670b3beb24c4bcee05fe89044779b07dfbb97
Sergio Enrique Barrios <se.checo34@gmail.com>
2022-12-08 01:35:32 -0600

    Cambios en la página de Inicio

--- index.php
+++ index.php
@@ -17,6 +17,8 @@
     <!-- Nuestro CSS -->
     <link rel="stylesheet" href="estilosIndex/styleMain.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bayon&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="estilosIndex/responsive.css">
     <script src="https://kit.fontawesome.com/1cd1525fc0.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
@@ -59,6 +61,10 @@
      <?php            
             }             
      ?>
+
+     <div class="thanks">
+      <h1>GRACIAS POR VISITARNOS. DISFUTE SU COMPRA</h1>
+     </div>
      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 </header>
